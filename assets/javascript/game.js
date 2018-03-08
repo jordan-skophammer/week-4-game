4bd1721483c769c2f705385213e5278fe6fca207
Jordan Skophammer <jordan.skophammer@gmail.com>
2018-03-08 07:01:31 -0600

    css done

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+// Jordan Skophammer's Cyrstal Collector Game, 2018
+
+
+
+$(document).ready(function(){
+	console.log('ready');
+})
+
+var createNumber = Math.floor(Math.random() * 101) + 19;
+
+$('.randomNumber').text(createNumber);
+
+
+	
+	
+
