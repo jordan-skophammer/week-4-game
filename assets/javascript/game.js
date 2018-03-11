5c6cabbde4a9d49bc5809848d9829162b8f01711
Jordan Skophammer <skop22@Jordan-Skophammers-iMac.local>
2018-03-11 16:27:57 -0500

    finished javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,13 +4,69 @@
 
 $(document).ready(function(){
 	console.log('ready');
-})
 
-var createNumber = Math.floor(Math.random() * 101) + 19;
+	var winsScore = 0;
+	var lossesScore = 0;
+	var playersScore = 0;
+	var createNumber = Math.floor(Math.random() * 100) + 20;
+	var crystalOne = Math.floor(Math.random() * 12) + 1;
+	var crystalTwo = Math.floor(Math.random() * 12) + 1;
+	var crystalThree = Math.floor(Math.random() * 12) + 1;
+	var crystalFour = Math.floor(Math.random() * 12) + 1;
 
-$('.randomNumber').text(createNumber);
+	$('.randomNumber').text(createNumber);
 
+	function gameSetup() {
+		var createNumber = Math.floor(Math.random() * 100) + 20;
+		var crystalOne = Math.floor(Math.random() * 12) + 1;
+		var crystalTwo = Math.floor(Math.random() * 12) + 1;
+		var crystalThree = Math.floor(Math.random() * 12) + 1;
+		var crystalFour = Math.floor(Math.random() * 12) + 1;
 
-	
-	
+		$('.randomNumber').text(createNumber);
 
+		playersScore = 0;
+		$('.scoreTile').text(playersScore);
+		
+	};
+
+	// gameSetup();
+
+	$('#crystal1').on("click", function() {
+		console.log(crystalOne);
+		runningScore(crystalOne);
+	})
+
+	$('#crystal2').on("click", function() {
+		console.log(crystalTwo);
+		runningScore(crystalTwo);
+	})
+
+	$('#crystal3').on("click", function() {
+		console.log(crystalThree);
+		runningScore(crystalThree);
+	})
+
+	$('#crystal4').on("click", function() {
+		console.log(crystalFour);
+		runningScore(crystalFour);
+	})
+
+	function runningScore(crystalVal) {
+		playersScore = playersScore + crystalVal;
+		$('.scoreTile').text(playersScore);
+		gameOutcome();
+	}
+
+	function gameOutcome() {
+		if(playersScore === createNumber) {
+			winsScore++
+			$('#wins').text(winsScore);
+			gameSetup();
+		} else if (playersScore > createNumber) {
+			lossesScore++;
+			$('#losses').text(lossesScore);
+			gameSetup();
+		}	
+	} 
+});
